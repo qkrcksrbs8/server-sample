6c541234f494676617de6f5338516f6b301af84c
changyun <qkrcksrbs8@naver.com>
2022-02-10 20:17:35 -0800

    루트화면, 로그인화면 임시 구현

--- app.js
+++ app.js
@@ -2,11 +2,42 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('기본 경로 응답입니다.');
+    res.send(
+        '\
+        <!DOCTYPE html>\
+        <html lang="ko">\
+        <head>\
+            <meta charset="UTF-8">\
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+            <title>Document</title>\
+        </head>\
+        <body>\
+            루트 화면입니다. <br>\
+        </body>\
+        </html>\
+        '
+    );
 });
 
 app.get('/login', (req, res) => {
-    res.send('여기는 로그인 페이지입니다.');
+    res.send(
+        '\
+        <!DOCTYPE html>\
+        <html lang="ko">\
+        <head>\
+            <meta charset="UTF-8">\
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+            <title>Document</title>\
+        </head>\
+        <body>\
+            로그인 화면입니다. <br>\
+            ID:<input type="text"><br>\
+            PW:<input type="text"></br>\
+            <button>로그인</button>\
+        </body>\
+        </html>\
+        '
+    );
 })
 
 app.listen(3000, () => {
