234a7fc848139e7c25b8e70dba900aeffc2b06b2
changyun <qkrcksrbs8@naver.com>
2022-02-10 18:38:45 -0800

    기본 경로 응답 구현 get : '/'

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('기본 경로 응답입니다.');
+});
+
 app.listen(3000, () => {
     console.log('서버가 시작되었습니다.');
 })
