03d4aaa14f26fd7b49e0a3678c64e84b3bf1db00
changyun <qkrcksrbs8@naver.com>
2022-02-10 20:36:36 -0800

    포트번호 재정의 -> 3000

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+"use strict";
+
+// 모듈
 const express = require('express');
 const app = express();
+
+// 포트
+const PORT = 3000;
+
+// 라우팅
+const home = require('./routes/home');
 
 // 앱 세팅
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-const home = require('./routes/home');
-app.use('/', home); // use -> 미들 웨어 등록
+// use -> 미들 웨어 등록
+app.use('/', home); 
 
-app.listen(3000, () => {
+// 서버 실행
+app.listen(PORT, () => {
     console.log('서버가 시작되었습니다.');
 })
