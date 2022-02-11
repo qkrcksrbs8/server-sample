682478b08044d3bbfca9ccc56c406148e2f0c8bb
changyun <qkrcksrbs8@naver.com>
2022-02-10 20:08:26 -0800

    로그인 페이지 임시 구현 삭제 로그인 경로만 유지

--- app.js
+++ app.js
@@ -6,25 +6,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.send(
-        '\
-        <!DOCTYPE html>\
-        <html lang="ko">\
-        <head>\
-            <meta charset="UTF-8">\
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-            <title>Document</title>\
-        </head>\
-        <body>\
-            로그인 화면입니다. <br>\
-            ID:<input type="text"><br>\
-            PW:<input type="text"></br>\
-            <button>로그인</button>\
-        </body>\
-        </html>\
-        '
-    );
-});
+    res.send('여기는 로그인 페이지입니다.');
+})
 
 app.listen(3000, () => {
     console.log('서버가 시작되었습니다.');
