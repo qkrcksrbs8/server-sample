55eab5e4b990badc77621bd85c3c78ac0c87c62c
changyun <qkrcksrbs8@naver.com>
2022-02-24 20:19:02 -0800

    인코딩 주석 추가

--- app/app.js
+++ app/app.js
@@ -13,6 +13,8 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`))
 app.use(bodyParser.json());
+
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // use -> 미들 웨어 등록
